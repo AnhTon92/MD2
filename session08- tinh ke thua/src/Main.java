7795caa92b6d0f40c91de26a3f82b67b197a8864
AnhTon92 <138435531+AnhTon92@users.noreply.github.com>
2024-03-14 08:23:21 +0700

    ss8

---
+++ session08- tinh ke thua/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    // biến instance
+    // thuộc tính
+//     int age = 100; // có thể sử dụng trực tiếp
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
